2f859b62a496faaef62311998d723cfd8e7b0737
John Rey Lumangyao <lumangyao112@gmail.com>
2024-02-19 14:43:29 +0800

    [misc][chappy] remove unused function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Patch, Param } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
 import { UserLoginDto } from './dto/User.dto';
@@ -15,10 +15,5 @@ export class UsersController {
   @Post('login')
   login(@Body() loginDto: UserLoginDto) {
     return this.usersService.login(loginDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: any) {
-    return this.usersService.update(+id, updateUserDto);
   }
 }
