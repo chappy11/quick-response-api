2a78045439a99aef307cbffff826cf5c47e41ec1
John Rey Lumangyao <lumangyao112@gmail.com>
2024-02-21 20:38:43 +0800

    [missc][chappy] Fix linting issue

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateAdminDto } from './dto/update-admin.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { Prisma } from '@prisma/client';
 import { AdminLoginDto } from './dto/admin.dto';
 
 @Injectable()
 export class AdminService {
-  constructor(private readonly dbServices: DatabaseService) {}
+  constructor(private readonly dbServices: DatabaseService) { }
 
   async create(createAdminDto: Prisma.AdminCreateInput) {
     const response = await this.dbServices.admin.create({
@@ -33,10 +32,6 @@ export class AdminService {
     });
 
     return response;
-  }
-
-  async update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
   }
 
   remove(id: number) {
